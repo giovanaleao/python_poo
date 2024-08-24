bd2d137b0a6d7cb1964e348d464fb18096a02b80
giovanaleao <giovanaleaogica062@gmail.com>
2024-08-23 21:11:02 -0300

    Alteração na classe Carro, inserção do consumo médio e nível de tanque.

--- src/main.py
+++ src/main.py
@@ -27,20 +27,25 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Informe o nível do tanque: '))
+    cm = float(input('Consumo médio: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     print('Cadastre o carro 2')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input('Informe o nível do tanque: '))
+    cm = float(input('Consumo médio: '))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro <600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and \
+            (carro1.tanque > 0 or carro2.tanque > 0):
         try:
             op_carro = 0
             while op_carro not in (1, 2):
